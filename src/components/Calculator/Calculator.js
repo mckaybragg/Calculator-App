41075dd7db575ea643f6fc42f3b4ec359dd09dd7
Adrian Bragg <adrianbragg0@gmail.com>
2019-11-06 13:41:48 -0600

    Added in the Calculator component and styled the background, gave it gradient and made the container for the calculator

---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Calculator.css';
+
+/* Main calculator component, contains our other calculator components. */
+class Calculator extends Component {
+    render() {
+        return (
+            <div className="Calculator">
+                
+            </div>
+        );
+    }
+}
+
+export default Calculator;
