504f7d2cd15a55ad4abcc3c9dbe0debc1027c156
Adrian Bragg <adrianbragg0@gmail.com>
2019-11-08 13:40:56 -0600

    Finished the claculator app

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -19,7 +19,52 @@ class Calculator extends Component {
 
     /* Function that is called when any of the keypad buttons are pressed */
     onButtonPress = (event) => {
-        console.log("onButtonPress called");
+        let equation = 
+        this.state.equation;
+        const pressedButtonValue =
+        event.target.innerHTML;
+
+        console.log(pressedButtonValue);
+
+        if (pressedButtonValue === 'C') {
+            this.clear();
+            return;
+        } else if ((pressedButtonValue >= '0' && pressedButtonValue <= '9') || pressedButtonValue === '.') {
+            /* If the user has pressed a button with a value between 0 and 9 or the period,
+            add that value to the equation */
+            equation += pressedButtonValue;
+        } else if (['+', '-', '*', '/', '%'].indexOf(pressedButtonValue) !== -1) {
+            /* If the user has pressed a button with the value of an operator, add the operator
+            to the equation with a space on either side */
+            equation += ' ' + pressedButtonValue + ' ';
+        } else if (pressedButtonValue === '=') {
+            /* If the user hits the enter button, calculate stuff */
+            /* Try catch blocks - try to run the code in the try block. If it fails and throws an error,
+            deal with it in the catch block */
+            try {
+                const evalResult = eval(equation);
+                const result = Number.isInteger(eval) ? evalResult : evalResult.toFixed(2);
+                this.setState({ result }); 
+            } catch (error) {
+                /* If an error occurs, print it to the console and alert the user */
+                console.log(error);
+                alert('An error ocurred. Check to make sure your equation is correct');
+            }
+        }  else {
+            /* If the user has pressed the back arrow, remove the last character from the equation string */
+            equation = equation.trim();
+            equation = equation.substr(0, equation.length -1);
+        }
+
+        /* Update our state with the new version of the equation */
+        this.setState({ equation });
+
+
+    }
+
+    clear() {
+        this.setState({ equation: '',
+        result: 0 });
     }
 
 
